7af9a74e7a33e2aeeec43de7c8b222fa1c313416
eshan-009 <eshansharma009@gmail.com>
2024-09-17 17:50:00 +0530

    preflightPatch

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ dbConnect();
 app.use(cors({
     credentials: true,
     origin: ['http://127.0.0.1:5500',`https://eshan-009.github.io/TodoApp_FrontEnd/`,`https://eshan-009.github.io`,`https://eshan-009.github.io/TodoApp_FrontEnd`,`https://eshan-009.github.io/`],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH','OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
  }))
 
