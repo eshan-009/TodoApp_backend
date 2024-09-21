d787805f6ca9c513b2e830bb8c0b41b18085191f
eshan-009 <eshansharma009@gmail.com>
2024-09-22 02:33:25 +0530

    Origin

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const PORT =process.env.PORT || 4000;
 dbConnect();
 app.use(cors({
     credentials: true,
-    origin: ['https://todo-app-react-seven-navy.vercel.app',`http://localhost:5173`,'http://127.0.0.1:5500',`https://eshan-009.github.io/TodoApp_FrontEnd/`,`https://eshan-009.github.io`,`https://eshan-009.github.io/TodoApp_FrontEnd`,`https://eshan-009.github.io/`],
+    origin: ['https://todo-app-react-kud8x13f7-eshan-sharmas-projects.vercel.app','https://todo-app-react-seven-navy.vercel.app',`http://localhost:5173`,'http://127.0.0.1:5500',`https://eshan-009.github.io/TodoApp_FrontEnd/`,`https://eshan-009.github.io`,`https://eshan-009.github.io/TodoApp_FrontEnd`,`https://eshan-009.github.io/`],
     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH','OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
  }))
